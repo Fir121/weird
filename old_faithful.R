76e2b1da6c40e2c4d105d7aaaa615e6f482fbd74
Rob Hyndman <robjhyndman@gmail.com>
2021-01-30 13:38:24 +1100

    Improved handling of old faithful data

--- old_faithful.R
+++ old_faithful.R
@@ -1,9 +1,11 @@
 library(tidyverse)
-oldfaithful <- read_tsv("data/Old_Faithful_eruptions.tsv") %>%
+oldfaithful <- "data/Old_Faithful_eruptions.tsv" %>%
+  read_tsv(col_types="dcidddddddddddcccdddciicccc") %>%
   mutate(
     seconds = as.numeric(duration_seconds),
-    time = as.POSIXct(eruption_time_epoch, origin="1970-01-01", tz="UTC")
-  ) 
+    time = as.POSIXct(eruption_time_epoch, origin="1970-01-01", tz="MST")
+  )
+# Note Yellowstone is Mountain Standard Time (UTC-7) most of the time.
 oldfaithful %>%
   filter(seconds > 7000) %>%
   select(time, duration, duration_seconds)
