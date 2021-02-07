5df71b64f32a6596723976a6aea5c6de72502151
Rob Hyndman <robjhyndman@gmail.com>
2021-02-07 16:58:32 +1100

    Some crime code

---
+++ crime.R
@@ -0,0 +1,23 @@
+# Downloaded from
+# https://ucr.fbi.gov/crime-in-the-u.s/2018/crime-in-the-u.s.-2018/topic-pages/offenses-known-to-law-enforcement
+# N0 data from Iowa
+library(tidyverse)
+
+# Table 8
+offences2018 <- readxl::read_excel(
+    here::here("data/crime/Table_8_Offenses_Known_to_Law_Enforcement_by_State_by_City_2018.xls"),
+    skip=3
+  ) %>%
+  janitor::clean_names() %>%
+  fill(state, .direction="down") %>%
+  filter(!str_detect(state,"^[0-9]")) %>%
+  filter(state != "IOWA7") %>%
+  mutate(across(violent_crime:arson2, ~ ./population*1e3))
+
+offences2018 %>%
+  ggplot(aes(x=violent_crime)) +
+  geom_boxplot()
+
+library(lookout)
+
+lookout(offences2018 %>% select(violent_crime) %>% filter(!is.na(violent_crime)))
