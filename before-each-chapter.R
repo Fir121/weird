17761f7d8417d85c12e665f67f648ca7d2392dbb
Rob J Hyndman <robjhyndman@gmail.com>
2021-10-04 15:40:39 +1100

    Mostly ch2

--- before-each-chapter.R
+++ before-each-chapter.R
@@ -23,10 +23,10 @@ options(
   width=58 + html*20,
   ggplot2.continuous.colour="viridis",
   ggplot2.continuous.fill = "viridis",
-  ggplot2.discrete.colour = c("#E69F00", "#56B4E9", "#009E73", "#CC79A7", "#D55E00", "#0072B2", "#F0E442"),
-  ggplot2.discrete.fill = c("#E69F00", "#56B4E9", "#009E73", "#CC79A7", "#D55E00", "#0072B2", "#F0E442")
+  ggplot2.discrete.colour = c("#D55E00", "#0072B2","#009E73", "#CC79A7", "#E69F00", "#56B4E9", "#F0E442"),
+  ggplot2.discrete.fill = c("#D55E00", "#0072B2","#009E73", "#CC79A7", "#E69F00", "#56B4E9", "#F0E442")
 )
-ggplot2::theme_set(theme_get() + theme(text = element_text(family = 'Carlito')))
+#ggplot2::theme_set(theme_get() + theme(text = element_text(family = 'Carlito')))
 
 # Avoid some conflicts
 conflicted::conflict_prefer("select","dplyr")
